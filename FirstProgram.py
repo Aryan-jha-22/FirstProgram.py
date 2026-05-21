7abdad3129f291df92ac2cf89d760dbb1d719ed8
Aryan-jha-22 <maabeta2008@gmail.com>
2026-05-21 16:18:51 +0530

    initial commit

---
+++ FirstProgram.py
@@ -0,0 +1,112 @@
+name = "Aryan"
+age = 17
+price = 19.99
+age2 = age
+print(name)
+print(age)
+print("my name is : ", name)
+print("my age is : ", age2)
+# This is a comment
+print("My name is : ", name, "and i am : ", age, "years old")
+print("My hobby is doing drawing", "I like learn python", "my aim is to qualify CSC scholarship at any how")
+print("The price of this book is : ", price)
+hobby = "drawing, listening songs"
+goal = "qualify csc scholarship, learn advanced data analyst course, get a internship till september and apply for scholarship in december"
+print("hello,my name is", name ,"my age is", age, "my hobbies are", hobby, "and my goal is", goal)
+print(type(name))
+print(type(age))
+print(type(price))
+name1 = 'AJ'
+name2 = "AJ"
+name3 = '''AJ'''
+print(name1)
+print(name2)
+print(name3)
+age = 18
+old = True
+a = None
+print(type(old))
+print(type(a))
+a = 25
+b = 34
+sum = a + b
+print(sum)
+diff = a - b
+print(diff)
+print("Hello World")
+#print("Hello World")
+'''
+Multi Line
+comment
+'''
+# print("Hello World")
+# print("Hello World")
+# print("Hello World")
+# Arthimetic operators
+a =  10
+b = 5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b) #always gives float value
+print(a%b) #remainder
+print(a**b) # a^b
+# Relational operators
+a = 50
+b = 10
+print(a == b) # == equal to (sign) FALSE
+print(a != b) # != not equal to (sign) True
+print(a >= b) # true
+print(a > b) # true
+print(a <= b) # false
+print(a < b) # false
+#assignment operators
+num = 10
+num = num + 10  # num += 10, num -= 10, num*= 10, num /= 10, num **= 5, num %= 10
+print("num :", num)
+#Logical operators
+print(not False)
+print(not True)
+a = 55
+b = 20
+print(not False)
+print(not (a > b))
+val1 = True
+val2 = False
+print("ans operator :", val1 and val2) #give true if both the value is true
+print("Or operator :", val1 or val2) #give true if any one of the value is true
+# type conversion
+a = 2
+b = 4.56
+sum = a + b # 2.0 + 4.56 = 6.56
+print(sum)
+a = int("2")
+b = 4.56
+print(a + b)
+a = 4.14
+a = str(a)
+print(type(a))
+input("enter your name : ")
+name = input("enter your name : ")
+print("Welcome", name)
+int("5")
+val = float(input("enter any value: "))
+print(type(val), val)
+name = input("enter your name: ")
+age = int(input("enter your age:"))
+marks = float(input("enter your marks: "))
+
+print("Welcome", name)
+print("your age is: ", age)
+print("marks: ", marks)
+first = input("enter first number: ")
+second = input("enter second number: ")
+print("sum= ", int(first) + int(second))
+side = input("side of square: ")
+print("area of square: ", int(side)*int(side))
+val1 = float(input("enter first number: "))
+val2 = float(input("enter second number: "))
+print("avg= ", (val1 + val2) / 2)
+a = int(input("first: ", ))
+b = int(input("second: ",))
+print(a >= b)
