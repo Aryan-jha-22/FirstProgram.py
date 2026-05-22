d7521537485a6da24d07416b2ab39948bc4ec478
Aryan-jha-22 <maabeta2008@gmail.com>
2026-05-22 11:56:42 +0530

    add initial implementation of string operations in lecture_two.py

---
+++ lecture_two.py
@@ -0,0 +1,11 @@
+str1 = "this is a string.\ti am learning python."
+str2 = 'apnacollege'
+str3 = """Aryan Jha"""
+print(str2 + "\t" + str3)
+final_str = "apna collegearyan jha"
+print(final_str)
+len1 = len(str2)
+print(len1)
+final_str = str2 + " " + str3
+print(final_str)
+print(len(final_str))
